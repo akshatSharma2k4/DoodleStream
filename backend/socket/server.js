058d81904606acdcbb18a85444de438d9b1a1dbd
Akshat Sharma <akshatsharma2k48@gmail.com>
2024-07-29 21:50:47 +0530

    Added favicon

--- backend/socket/server.js
+++ backend/socket/server.js
@@ -4,15 +4,16 @@ const { Server } = require("socket.io");
 const http = require("http");
 const cors = require("cors");
 const getWords = require("../helper/getWords.js");
+const dotenv = require("dotenv").config();
 app.use(
     cors({
-        origin: "*",
+        origin: process.env.FRONTEND_URL,
     })
 );
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: process.env.FRONTEND_URL,
     },
 });
 
@@ -42,12 +43,6 @@ const calcuatePointsForGuess = (guessTime, totalGuessesBefore) => {
     return finalPoints;
 };
 
-// const calcuatePointsForGuess = () => {
-//     return 1;
-// };
-// const calculatePointsForDraw = () => {
-//     return 1;
-// };
 
 const roundUpdation = (room, currentRoundValue) => {
     if (currentRoundValue == roomConditions[room]?.rounds) {
