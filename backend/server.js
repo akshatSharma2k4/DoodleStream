951a15bb17b5323afa6eb0f785fd4c155770f2ff
Akshat Sharma <akshatsharma2k48@gmail.com>
2024-08-11 19:38:34 +0530

    Added build command in root

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const { server } = require("./socket/server");
 const { app } = require("./socket/server");
-const dotenv = require("dotenv").config();
+const path = require("path");
+const dotenv = require("dotenv").config({
+    path: path.resolve(__dirname, ".env"),
+});
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
