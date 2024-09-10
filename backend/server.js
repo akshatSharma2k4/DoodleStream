3e34fbfa78fefe84181eb94a0a2dca2e97cc8e91
Akshat Sharma <akshatsharma2k48@gmail.com>
2024-09-10 17:29:27 +0530

    Vercel

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const { server } = require("./socket/server");
 const { app } = require("./socket/server");
 const path = require("path");
+console.log(path.resolve(__dirname, "../.env"));
 const dotenv = require("dotenv").config({
-    path: path.resolve(__dirname, ".env"),
+    path: path.resolve(__dirname, "../.env"),
 });
 const PORT = process.env.PORT || 8080;
 
